272d72a68c597152892b0d60fbd11af4c172104f
grad0ff <grad0ff@mail.ru>
2022-07-03 01:44:46 +0300

    added notifications, remote webdriver

--- src/test/java/com/itoolabs/index_page_tests/TestBase.java
+++ src/test/java/com/itoolabs/index_page_tests/TestBase.java
@@ -6,6 +6,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -15,10 +16,8 @@ public class TestBase {
     @BeforeAll
     static void initTests() {
         Configuration.baseUrl = "https://itoolabs.com";
-        Configuration.browserPosition = "1700x0";
-        Configuration.browserSize= "1920x1080";
-//        Configuration.holdBrowserOpen = true;
         SelenideLogger.addListener("allure", new AllureSelenide());
+        setRemoteWebdriver();
 
     }
 
@@ -33,5 +32,13 @@ public class TestBase {
     static void finishTests() {
         closeWebDriver();
         clearBrowserCookies();
+    }
+
+    static void setRemoteWebdriver() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
